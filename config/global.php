a73f8c32cacb5228ad60aa64c2795fd268e3eb63
marlonSamuel <marlongf2195@gmail.com>
2024-02-05 21:00:43 -0600

    antiguas

--- config/global.php
+++ config/global.php
@@ -3,20 +3,20 @@
 define("DB_HOST","143.198.156.48");
 
 //Nombre de la base de datos
-define("DB_NAME", "new_horizon_db");
+define("DB_NAME", "mcking_db");
 
 //Usuario de la base de datos
-define("DB_USERNAME", "root");
+define("DB_USERNAME", "kim");
 
 //Contraseña del usuario de la base de datos
-define("DB_PASSWORD", "new-horizon-db");
+define("DB_PASSWORD", "Kimberly123#");
 //define("DB_PASSWORD", "Km0r3N0123@@");
 
 //definimos la codificación de los caracteres
 define("DB_ENCODE","utf8");
 
 //Definimos una constante como nombre del proyecto
-define("PRO_NOMBRE","NEW HORIZON");
+define("PRO_NOMBRE","Macking");
 
 //Ruta del servidor
 //define("RUTA_SERVER", "/var/www/html/macking_sis");
